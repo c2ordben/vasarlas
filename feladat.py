3fc130472d9dce146bfea5277015b4a91f303d77
c2ordben <c2ordben@vasvari.org>
2025-02-13 11:36:24 +0100

    1. ora vege

--- feladat.py
+++ feladat.py
@@ -1,16 +1,22 @@
 f = open('vasarlas.csv', 'r')
-nulla = 0
 
-def koltott():
+for szam in f:
+    szam = szam.strip()
+    tmp = szam.split(";")
+
+def koltott(tmp):
     napSzam = 0
-    for szam in f:
-        szam = szam.strip()
-        tmp = szam.split(";")
-        for nap in tmp:
-            napSzam +=1
-        print(napSzam,'nap volt a hónapban')
-
-koltott()
+    for nap in tmp:
+        napSzam +=1
+    print(napSzam,'nap volt a hónapban')
 
 
+def nincsKoltes(tmp):
+    nulla = 0
+    for nap in tmp:
+        if nap == 0:
+            nulla += 1
+        print(nulla, 'nap nem volt költés')
 f.close
+koltott(tmp)
+nincsKoltes(tmp)
